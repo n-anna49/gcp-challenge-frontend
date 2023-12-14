096519f3096e7db409ab4d6b3a806b970bf10f29
n-anna49 <105296323+n-anna49@users.noreply.github.com>
2023-12-14 13:24:28 -0700

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,12 @@
+fetch('https://mygateway-6blow143.wl.gateway.dev/')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    const visitorCount = data['Visitor Count'];
+	const dataContainer = document.getElementById('visitor-count');
+    dataContainer.innerHTML = `${visitorCount}`;
+  })
+  .catch(error => {
+    console.error('Error fetching JSON data:', error);
+  });
